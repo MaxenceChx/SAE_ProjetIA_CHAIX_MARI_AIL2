b80a55e7e7c8185ea1a39310ae001ef5923dbc8b
Maxence C. <maxchaix54@gmail.com>
2024-06-19 09:04:16 +0200

    Update AlgoClustering.java

--- SAÉ/src/Algorithmes/AlgoClustering.java
+++ SAÉ/src/Algorithmes/AlgoClustering.java
@@ -1,6 +1,13 @@
 package Algorithmes;
 
 public interface AlgoClustering {
-    //méthode génerique pour le clustering
-    public int[] clustering(int[][] valeurs);
+    /**
+     * Applique l'algorithme de clustering sur les données fournies.
+     *
+     * @param data Un tableau 2D de taille (nombre d'objets, nombre de caractéristiques)
+     *             qui contient la description des objets à classifier.
+     * @param nbClusters Le nombre de clusters souhaités.
+     * @return Un tableau 1D de taille (nombre d'objets) qui contient un numéro de cluster pour chaque objet.
+     */
+    int[] clustering(double[][] data, int nbClusters);
 }
